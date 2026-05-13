0bac1d51c5ddd7334f7b67d8100bb5ac14fab425
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2026-05-13 13:04:19 +0200

    Update DHCP server configuration for new subnet

--- GIP Cybersecurity/WSDOC01/ad dhcp.ps1
+++ GIP Cybersecurity/WSDOC01/ad dhcp.ps1
@@ -10,7 +10,7 @@ Add-DhcpServerv4Scope `
 Set-DhcpServerv4OptionValue `
 -ScopeId 10.38.1.0 `
 -Router 10.38.1.254 `
--DnsServer 8.8.8.8,1.1.1.1 `
+-DnsServer 127.0.0.1 `
 -DnsDomain "localdomain"
 
 # Exclusion range toevoegen
@@ -22,20 +22,20 @@ Add-DhcpServerv4ExclusionRange `
 # DHCP Scope aanmaken
 Add-DhcpServerv4Scope `
 -Name "Wireless" `
--StartRange 10.38.1.10 `
--EndRange 10.38.1.200 `
+-StartRange 10.38.2.1 `
+-EndRange 10.38.2.249 `
 -SubnetMask 255.255.255.0 `
 -State Active
 
 # DHCP opties instellen
 Set-DhcpServerv4OptionValue `
--ScopeId 10.38.1.0 `
--Router 10.38.1.254 `
--DnsServer 8.8.8.8,1.1.1.1 `
+-ScopeId 10.38.2.0 `
+-Router 10.38.2.254 `
+-DnsServer 127.0.0.1 `
 -DnsDomain "localdomain"
 
 # Exclusion range toevoegen
 Add-DhcpServerv4ExclusionRange `
--ScopeId 10.38.1.0 `
--StartRange 10.38.1.250 `
--EndRange 10.38.1.253
+-ScopeId 10.38.2.0 `
+-StartRange 10.38.2.250 `
+-EndRange 10.38.2.253
