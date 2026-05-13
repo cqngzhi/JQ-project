d515dcda5e98581f9662f98ea074a11576d29bff
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2026-05-13 12:56:18 +0200

    Update DHCP scope creation parameters

---
+++ GIP Cybersecurity/WSDOC01/ad dhcp.ps1
@@ -0,0 +1,41 @@
+# DHCP Scope aanmaken
+Add-DhcpServerv4Scope `
+-Name "Client" `
+-StartRange 10.38.1.1 `
+-EndRange 10.38.1.249 `
+-SubnetMask 255.255.255.0 `
+-State Active
+
+# DHCP opties instellen
+Set-DhcpServerv4OptionValue `
+-ScopeId 10.38.1.0 `
+-Router 10.38.1.254 `
+-DnsServer 8.8.8.8,1.1.1.1 `
+-DnsDomain "localdomain"
+
+# Exclusion range toevoegen
+Add-DhcpServerv4ExclusionRange `
+-ScopeId 10.38.1.0 `
+-StartRange 10.38.1.250 `
+-EndRange 10.38.1.253
+
+# DHCP Scope aanmaken
+Add-DhcpServerv4Scope `
+-Name "LAN_SCOPE" `
+-StartRange 10.38.1.10 `
+-EndRange 10.38.1.200 `
+-SubnetMask 255.255.255.0 `
+-State Active
+
+# DHCP opties instellen
+Set-DhcpServerv4OptionValue `
+-ScopeId 10.38.1.0 `
+-Router 10.38.1.254 `
+-DnsServer 8.8.8.8,1.1.1.1 `
+-DnsDomain "localdomain"
+
+# Exclusion range toevoegen
+Add-DhcpServerv4ExclusionRange `
+-ScopeId 10.38.1.0 `
+-StartRange 10.38.1.250 `
+-EndRange 10.38.1.253
